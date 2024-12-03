00fe6742d2c2a450977d9d9965c08e579f2fb902
vinuth <vinuthsriarampath@outlook.com>
2024-12-03 07:03:32 +0530

    initial commit

---
+++ android/app/src/main/kotlin/edu/vinuth/mobiledev/awsome_places/awsome_places/MainActivity.kt
@@ -0,0 +1,5 @@
+package edu.vinuth.mobiledev.awsome_places.awsome_places
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/util/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+ final Color mainTextColor=const Color(0xff3B3636).withOpacity(0.75);
+ const Color mainColor=Color(0xff9E00FF);
+ const Color mainBlack=Colors.black;
+ const Color mainWhite=Colors.white;
+
+ const Color firstCategoryColor=Color(0xff8E8FFA);
+ const Color secondCategoryColor=Color(0xffC2D9FF);
+ const Color thirdCategoryColor=Color(0xffFFE500);
+ 
+ const Color mainNaturalWondersColor=Color(0xff2EB200);
+ final Color subNaturalWondersColor=const Color(0xff2EB200).withOpacity(0.33);
+
+ const Color mainNightLifeColor=Color(0xffFF9900);
+ final Color subNightLifeColor=const Color(0xffFF9900).withOpacity(0.33);
+
+const Color mainLandmarksColor=Color(0xff0029FF);
+final Color subLandmarksColor=const Color(0xff0029FF).withOpacity(0.33);
+final Color landmarksBgColor=const Color(0xffCACACA).withOpacity(0.31);
+
+const Color mainCultureColor=Color(0xffC00C00);
+
+const Color addButtonColor=Color(0xff06FFA5);
+const Color removeButtonColor=Color(0xffFF1E0F);
